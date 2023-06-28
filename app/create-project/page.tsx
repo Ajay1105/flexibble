213a42182a42077fef4380be11ac93ca3011eec7
Ajay <aky11052003@gmail.com>
2023-06-28 11:03:58 +0530

    bug fixed

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -9,7 +9,7 @@ const CreateProject = async() => {
   return (
     <Modal>
         <h3 className="modal-head-text"> Create a New Project</h3>
-        <ProjectForm type='create' session={session}/>
+        <ProjectForm type='create' session={session} />
     </Modal>
   )
 }
